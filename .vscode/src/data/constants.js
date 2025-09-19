07bd3251e12def975e371cfb634a6d30cdcbd579
Harini A <harini.2205036@srec.ac.in>
2025-09-19 14:16:19 +0530

    first commit

---
+++ .vscode/src/data/constants.js
@@ -0,0 +1,223 @@
+export const Bio = {
+  name: "HARINI A",
+  roles: [
+    "Full Stack Developer",
+    "UI/UX Designer",
+    "Programmer",
+  ],
+  description:
+    "Dedicated and motivated Information Technology student with a strong foundation in software development. Possesses excellent problem-solving skills and a keen interest in emerging technologies",
+  github: "https://github.com/rishavchanda",
+  resume:
+    "https://drive.google.com/file/d/1K5IMatGS9ZFtn5JLhkdopQuyDCo2YnFI/view?usp=drive_link",
+  linkedin: "http://www.linkedin.com/in/harini-a-0744b2296/",
+  GitHub: "https://github.com/hariniarulkumaran",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "Next Js",
+        image:
+          "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAACTklEQVR4Ab1XAaQqURB9DyohSykREpRIQSAlBCoECKUFCSRCBBEAaSEABQEoCIEASCwAUICALgCo83do0//9v819XX845O7VnDkzOzP7JWGaBd3C3IJpQVjAHeJ+Rs9a97vKLGrBsB1KgMhEP3FMUUwt4ENMfxr1yQIU4SSjRkbeOZtERmHk6pXQVDlnkHh9S+QLTm1hkiz4n/gzFQuny9FoFLquE+i34x+n02k0m00UCoV3BIzn3MMJrVYLtp1OJ0cS/X4f5/MZhmG8IyDsWtDfEaDIn2232/3zbrvdxuFwwGg04qRBt+VnETBNE0IIkE2n07/erdfrWK/X6Ha73Hb9ZXII3G43ivy3dNRqtZe7lUoFs9mM6oBDwCQCgquALT1FT3a5XF7qIZ/PYzgcolqtcggIIgBZAgRKB6lCRalp2uM8k8mAVMrlchwC+DEBipycE4n5fP44j8ViKJVKSCaTbAJCpgaez4vFIsjoWa/XA50FAgEkEgmEw2F2CkxZBZ5Br5tt1ITcbjd8Ph88Hg+7CBefECCsVitS4aVJcV9D/VMCVITk/Hq9YrPZyBBo2a1YMGvAcQYcj0cCtWMugcdYNhjDiBrP25mx3++x3W6RzWZZ8isfxzQLlsslJpMJpYY5jhkqcOH1ejEYDDAej9FoNOByuZxGsfqVzC7KTqcDSkkqleKsZOqX0mAwiHK5DGrJfr+fs5SqX8sjkQji8ThCoRC+v78Za7l6JagrUh3YkUuZpqgwDaecc9VYSDoV5Fg+at7n+eLN57kuE/EvzHr/Kvs31aYAAAAASUVORK5CYII=",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Tailwind css",
+        image:
+          "https://recursoswebyseo.com/wp-content/uploads/2023/10/tailwind-css-jpeg.webp",
+      },
+    ],
+  },
+  {
+    title: "Tools and Technologies",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://static.vecteezy.com/system/resources/previews/017/119/660/original/github-logo-git-hub-icon-with-text-on-white-and-black-background-free-vector.jpg",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Canva",
+        image:
+          "https://logohistory.net/wp-content/uploads/2023/07/Canva-Emblem.png",
+      },
+      {
+        name: "Android Studio",
+        image:
+          "https://androidayuda.com/wp-content/uploads/2022/07/android-studio.jpg",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Google Colab",
+        image:
+         "https://colab.research.google.com/img/colab_favicon_256px.png",
+      },
+      {
+        name: "Figma",
+        image:
+        "https://facts.net/wp-content/uploads/2023/09/15-astounding-facts-about-figma-1695676043.jpg"
+        },
+    ],
+  },
+  {
+    title: "Programming Language",
+    skills: [
+      {
+        name: "java",
+        image: "https://wp.logos-download.com/wp-content/uploads/2016/10/Java_logo_icon.png",
+      },
+      {
+        name: "C",
+        image:"https://static.skillshare.com/uploads/video/thumbnails/b9455fc40a4053509ef0a77b8ddb6a51/original",
+      },
+    ],
+  },
+  {
+    title: "Database",
+    skills: [
+      {
+        name: "Mysql",
+        image:
+          "https://huynhthaihung.com/wp-content/uploads/2019/07/MySQL-Crack.jpg",
+      },
+      {
+        name: "Mongodb",
+        image:
+          "https://miro.medium.com/v2/resize:fit:1358/0*AO3r9XWmmzo9ws-T.png",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://i.postimg.cc/cHvxL1tt/Screenshot-2025-09-18-221930.png",
+    role: "Frontend Developer",
+    company: "Blinking Soft - Coimbatore",
+    date: "June 10 - July 10 (2024)",
+    skills: [
+     "Developed a job portal using the MERN stack, implementing tailored functionalities for distinct user types to enhance engagement.",
+     "Responsive UI with Next.js and Tailwind CSS.",
+     "Designed the logo for the company's job portal product, reflecting its user-centric features and enhancing brand visibility and recognition in the recruitment industry.",
+    ],
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://noticebard.com/wp-content/uploads/2022/08/SNR.png",
+    college: "•	Sri Ramakrishna Engineering College, Coimbatore, Tamil Nadu",
+    date: "2022-2026",
+    grade: "7.83 CGPA",
+    degree: "Bachelor of Technology - BTech, Information Technology",
+  },
+  {
+    id: 1,
+    img: "https://1.bp.blogspot.com/-GMpqjySBak4/X41WKln22DI/AAAAAAAA8rM/xlX8wNWyrEQgH4xJbo8ZUpLdisFkvxsbgCNcBGAsYHQ/s336/Bharathi%2BMHSS%2Blogo.png",
+    school: "Bharathi Matriculation Higher Secondary School, Coimbatore, TN",
+    date: "2020",
+    grade: "67%",
+  },
+   {
+    id: 2,
+    img: "https://1.bp.blogspot.com/-GMpqjySBak4/X41WKln22DI/AAAAAAAA8rM/xlX8wNWyrEQgH4xJbo8ZUpLdisFkvxsbgCNcBGAsYHQ/s336/Bharathi%2BMHSS%2Blogo.png",
+    school: "Bharathi Matriculation Higher Secondary School, Coimbatore, TN",
+    date: "2022",
+    grade: "73.5%",
+  },
+];
+export const projects = [
+  {
+    id: 0,
+    title: "AI-PLATFORM FOR SCHEME ASSISTANCE",
+    description:[
+      "Developed a multilingual government scheme chatbot using Python, Streamlit, Flask, and Retrieval-Augmented Generation (RAG) to provide accurate answers from official PDFs through semantic search and summarization.",
+      "Implemented real-time language detection, automatic translation, and vector-based query matching using Google Translator API, LangDetect, and embedding models, enabling inclusive access across Indian languages.",
+      "Designed and deployed an interactive frontend on Streamlit, integrating backend logic via RESTful Flask APIs to support dynamic Q&A, voice/text input, and personalized user experience.",
+
+    ],
+    image: "https://i.postimg.cc/V6p4sLmt/Screenshot-2025-08-02-162745.png", 
+    "tags": [
+  "Python",
+  "Streamlit",
+  "Flask",
+  "Google Translate API",
+  "LangDetect",
+  "Retrieval-Augmented Generation (RAG)",
+  "Semantic Search",
+  "Vector Embeddings",
+  "PDF Text Extraction",
+  "Text Summarization",
+  "Multilingual Chatbot",
+  "REST API",
+  "Natural Language Processing",
+  "AI Chatbot",
+  "Government Schemes"
+],
+    category: "web app",
+    github: "https://github.com/hariniarulkumaran/SarkarAI", // Replace with your repo
+    member: [
+      {
+        name: "Harini A",
+        linkedin: "http://www.linkedin.com/in/harini-a-0744b2296/", // Replace with your LinkedIn
+        github: "https://github.com/hariniarulkumaran", // Replace with your GitHub
+      },
+    ],
+  },
+  {
+    id: 1,
+    title: "Expense Tracker App",
+    description:
+     [
+     "Developed a dynamic expense tracking web app using HTML, CSS, JavaScript, and local Storage, enabling realtime transaction logging and persistent data storage.",
+"Implemented interactive category filters, total calculation logic, and responsive UI with Tailwind CSS for seamless mobile and desktop usage.",
+"Used DOM manipulation and array methods (map, filter, reduce) for real-time data handling and visual updates, enhancing user interactivity.",
+     ],
+    image: "https://i.postimg.cc/4dgVW7fB/Screenshot-2025-08-06-190324.png", // Replace with your image link
+    tags: ["JavaScript", "HTML", "CSS"],
+    category: "web app",
+    github: "https://github.com/hariniarulkumaran/ExpenseTrackerApp-p1-", // Replace with your repo
+    webapp: "https://expense-tracker-harini.netlify.app", // Replace with your deployed link
+    member: [
+      {
+        name: "Harini A",
+       
+        linkedin: "http://www.linkedin.com/in/harini-a-0744b2296/", // Replace with your LinkedIn
+        github: "https://github.com/hariniarulkumaran", // Replace with your GitHub
+      },
+    ],
+  },
+   
+];
